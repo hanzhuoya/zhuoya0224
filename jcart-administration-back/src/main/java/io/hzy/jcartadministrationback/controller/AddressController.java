4492121f0c3c98649d7a805de16e0a1b30caa7a1
zhuoya <2602793764@qq.com>
2020-02-25 21:54:56 +0800

    add administration address api

---
+++ jcart-administration-back/src/main/java/io/hzy/jcartadministrationback/controller/AddressController.java
@@ -0,0 +1,18 @@
+package io.hzy.jcartadministrationback.controller;
+
+import io.hzy.jcartadministrationback.dto.out.AddressListOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+
+    @GetMapping("/getListByCustomerId")
+    public List<AddressListOutDTO> getListByCusttomerId(){
+        return null;
+    }
+}

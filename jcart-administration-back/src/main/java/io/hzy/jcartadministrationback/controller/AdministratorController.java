a5c4481dd3b9b3b0c6f56ff5ce7408fc76f4adf3
zhuoya <2602793764@qq.com>
2020-02-25 18:39:09 +0800

    add some administrator apis

--- jcart-administration-back/src/main/java/io/hzy/jcartadministrationback/controller/AdministratorController.java
+++ jcart-administration-back/src/main/java/io/hzy/jcartadministrationback/controller/AdministratorController.java
@@ -1,12 +1,12 @@
 package io.hzy.jcartadministrationback.controller;
 
 import io.hzy.jcartadministrationback.dto.in.AdministratorLoginDTO;
+import io.hzy.jcartadministrationback.dto.in.AdministratorResetPwdInDTO;
 import io.hzy.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
 import io.hzy.jcartadministrationback.dto.out.AdministratorGetProfileOutDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.hzy.jcartadministrationback.dto.out.AdministratorListOutDTO;
+import io.hzy.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("administrator")
@@ -18,12 +18,27 @@ public class AdministratorController {
     }
 
     @PostMapping("/getProfile")
-    public AdministratorGetProfileOutDTO getProfile(Integer adminstratorId){
+    public AdministratorGetProfileOutDTO getProfile(@RequestParam(required = false) Integer adminstratorId){
         return null;
     }
 
     @PostMapping("/updateProdfile")
     public void updateProdfile(AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
 
+    }
+
+    @GetMapping("/getPwdResetCode")
+    public String getPwdResetCode(@RequestParam String email){
+        return null;
+    }
+
+    @PostMapping("/resetPwd")
+    public void resetPwd(@RequestBody AdministratorResetPwdInDTO administratorResetPwdInDTO){
+
+    }
+
+    @GetMapping("/getList")
+    public PageOutDTO<AdministratorListOutDTO> getList(@RequestParam Integer pageNum){
+      return null;
     }
 }
---
+++ jcart-administration-back/src/main/java/io/hzy/jcartadministrationback/dto/out/AdministratorListOutDTO.java
@@ -0,0 +1,40 @@
+package io.hzy.jcartadministrationback.dto.out;
+
+public class AdministratorListOutDTO {
+    private Integer administratorIs;
+    private String username;
+    private Byte status;
+    private Long createTimestamp;
+
+    public Integer getAdministratorIs() {
+        return administratorIs;
+    }
+
+    public void setAdministratorIs(Integer administratorIs) {
+        this.administratorIs = administratorIs;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public Long getCreateTimestamp() {
+        return createTimestamp;
+    }
+
+    public void setCreateTimestamp(Long createTimestamp) {
+        this.createTimestamp = createTimestamp;
+    }
+}

1407c2c9004d0bad6a530fe431906eebcd0836de
zhuoya <2602793764@qq.com>
2020-02-26 10:42:05 +0800

    add administration return apis

---
+++ jcart-administration-back/src/main/java/io/hzy/jcartadministrationback/controller/ReturnController.java
@@ -0,0 +1,32 @@
+package io.hzy.jcartadministrationback.controller;
+
+import io.hzy.jcartadministrationback.dto.in.ReturnSearchInDTO;
+import io.hzy.jcartadministrationback.dto.in.ReturnUpdateActionInDTO;
+import io.hzy.jcartadministrationback.dto.out.PageOutDTO;
+import io.hzy.jcartadministrationback.dto.out.ReturnListOutDTO;
+import io.hzy.jcartadministrationback.dto.out.ReturnShowOutDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/return")
+public class ReturnController {
+
+    @GetMapping("/search")
+    public PageOutDTO<ReturnListOutDTO> search(ReturnSearchInDTO returnSearchInDTO, @RequestParam Integer pageNum){
+        return null;
+    }
+
+
+    @GetMapping("/getById")
+    public ReturnShowOutDTO getById(@RequestParam Integer returnId){
+       return null;
+    }
+
+    @GetMapping("/updateAction")
+    public void updateAction(@RequestParam ReturnUpdateActionInDTO returnUpdateActionInDTO){
+        
+    }
+}
